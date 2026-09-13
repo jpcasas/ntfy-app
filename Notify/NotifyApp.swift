1a3592303647e71a626a6ba28dd971e697c03dfe
Juan <jpcasas@gmail.com>
2026-09-13 11:00:00 +0200

    Initial Commit

---
+++ Notify/NotifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotifyApp.swift
+//  Notify
+//
+//  Created by Juan on 2026-09-13.
+//
+
+import SwiftUI
+
+@main
+struct NotifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
